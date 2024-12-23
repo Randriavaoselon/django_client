b3c7a042af0ff6c16fdd6eb6997dd8ad80089a23
Randriavaoselon <selonrandriavao209@gmail.com>
2024-12-23 17:21:58 +0300

    config deploiementB

--- socketClient/settings.py
+++ socketClient/settings.py
@@ -17,7 +17,7 @@ from pathlib import Path
 from dotenv import load_dotenv
 from decouple import config
 
-load_dotenv() #load env variable
+#load_dotenv() #load env variable
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,9 +37,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = True
+DEBUG = True
 
-DEBUG = os.environ.get('DEBUG')
+#DEBUG = os.environ.get('DEBUG')
 
 ALLOWED_HOSTS = ['*']
 
@@ -94,12 +94,12 @@ WSGI_APPLICATION = 'socketClient.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 DATABASES = {
     
